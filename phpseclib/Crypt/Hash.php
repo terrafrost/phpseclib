b4e63afdabeeab4e041075a00a058ff869fd3a02
terrafrost <terrafrost@php.net>
2015-09-06 02:08:58 -0500

    ...

--- phpseclib/Crypt/Hash.php
+++ phpseclib/Crypt/Hash.php
@@ -164,6 +164,7 @@ class Hash
                     $this->l = abs($matches[1]) >> 3;
                     break;
                 }
+print_r(hash_algos()); exit;
                 throw new UnsupportedAlgorithmException("$hash is not a supported algorithm");
         }
 
