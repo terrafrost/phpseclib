ecff3b37ef5156a4895f94bb479e155e6c769782
terrafrost <terrafrost@php.net>
2015-09-15 19:43:59 -0500

    RSA: update unit tests to account for latest code changes

---
+++ tests/Unit/Crypt/RSA/CreateKeyTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author    Jim Wigginton <terrafrost@php.net>
+ * @copyright 2013 Jim Wigginton
+ * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
+ */
+
+use phpseclib\Crypt\RSA;
+
+class Unit_Crypt_RSA_CreateKeyTest extends PhpseclibTestCase
+{
+    static protected $privatekey;
+    static protected $publickey;
+
+    public function testCreateKey()
+    {
+        extract(RSA::createKey(512));
+        $this->assertInstanceOf('RSA', $privatekey);
+        $this->assertInstanceOf('RSA', $publickey);
+        $this->assertNotEmpty("$privatekey");
+        $this->assertNotEmpty("$publickey");
+
+        self::$publickey = $publickey;
+        self::$privatekey = $privatekey;
+    }
+
+    /**
+     * @depends testCreateKey
+     */
+    public function testEncryptDecrypt()
+    {
+        $ciphertext = self::$publickey->encrypt('zzz');
+        $this->assertInternalType('string', $ciphertext);
+        $plaintext = self::$publickey->decrypt($ciphertext);
+        $this->assertSame($plaintext, 'zzz');
+    }
+}
