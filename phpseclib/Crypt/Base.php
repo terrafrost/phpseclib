026592d2bbacc283fa230a1faa33a94bf745893a
terrafrost <terrafrost@php.net>
2015-09-18 00:21:03 -0500

    ...

--- phpseclib/Crypt/Base.php
+++ phpseclib/Crypt/Base.php
@@ -1914,8 +1914,6 @@ class Crypt_Base
         // $this->encryptIV = $this->decryptIV = strlen($this->iv) == $this->block_size ? $this->iv : str_repeat("\0", $this->block_size);
         $this->encryptIV = $this->decryptIV = str_pad(substr($this->iv, 0, $this->block_size), $this->block_size, "\0");
 
-echo 'orig key size = '.strlen($this->key) . "\n";
-echo 'forced key size = '.$this->key_size . "\n";
         $this->key = str_pad(substr($this->key, 0, $this->key_size), $this->key_size, "\0");
     }
 
