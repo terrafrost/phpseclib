595eecccace79c3e00dedd00d0e2803f456701c4
terrafrost <terrafrost@php.net>
2015-10-01 13:46:23 -0500

    aaa

--- phpseclib/Crypt/RSA/XML.php
+++ phpseclib/Crypt/RSA/XML.php
@@ -128,7 +128,8 @@ class XML
      */
     static function savePublicKey(BigInteger $n, BigInteger $e)
     {
-echo "\n\n\n" . base64_encode($n->toBytes()) . "\n\n\n";
+echo 'ZZZ = '.base64_encode('ZZZ');
+echo "\n\n\n" . base64_encode($n->toBytes(true)) . "\n\n\n";
         return "<RSAKeyValue>\r\n" .
                '  <Modulus>' . base64_encode($n->toBytes()) . "</Modulus>\r\n" .
                '  <Exponent>' . base64_encode($e->toBytes()) . "</Exponent>\r\n" .
