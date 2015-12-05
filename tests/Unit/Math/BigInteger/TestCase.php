a016832b669091a00845d65374a30f26a7c68ea3
terrafrost <terrafrost@php.net>
2015-12-04 22:30:23 -0600

    ...

--- tests/Unit/Math/BigInteger/TestCase.php
+++ tests/Unit/Math/BigInteger/TestCase.php
@@ -365,7 +365,7 @@ abstract class Unit_Math_BigInteger_TestCase extends PhpseclibTestCase
     public function testDebugInfo()
     {
         $num = new Math_BigInteger(50);
-        //$str = var_dump($num);
+        $str = print_r($num, true);
         $this->assertContains('[value] => 0x32', $str);
         return $str;
     }
