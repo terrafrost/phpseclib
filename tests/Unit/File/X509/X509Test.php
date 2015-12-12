56ab489b099fee9562b1e97e4958a7a70f702147
terrafrost <terrafrost@php.net>
2015-12-11 22:51:00 -0600

    ...

--- tests/Unit/File/X509/X509Test.php
+++ tests/Unit/File/X509/X509Test.php
@@ -9,36 +9,5 @@ require_once 'File/X509.php';
 
 class Unit_File_X509_X509Test extends PhpseclibTestCase
 {
-    private function _encodeOID($oid)
-    {
-        if ($oid === false) {
-            user_error('Invalid OID');
-            return false;
-        }
-        $value = '';
-        $parts = explode('.', $oid);
-        $value = chr(40 * $parts[0] + $parts[1]);
-        for ($i = 2; $i < count($parts); $i++) {
-            $temp = '';
-            if (!$parts[$i]) {
-                $temp = "\0";
-            } else {
-                while ($parts[$i]) {
-                    $temp = chr(0x80 | ($parts[$i] & 0x7F)) . $temp;
-                    $parts[$i] >>= 7;
-                }
-                $temp[strlen($temp) - 1] = $temp[strlen($temp) - 1] & chr(0x7F);
-            }
-            $value.= $temp;
-        }
-        return $value;
-    }
 
-    public function testGetOID()
-    {
-        $x509 = new File_X509();
-        $this->assertEquals($x509->getOID('2.16.840.1.101.3.4.2.1'), '2.16.840.1.101.3.4.2.1');
-        $this->assertEquals($x509->getOID('id-sha256'), '2.16.840.1.101.3.4.2.1');
-        $this->assertEquals($x509->getOID('zzz'), 'zzz');
-    }
 }
