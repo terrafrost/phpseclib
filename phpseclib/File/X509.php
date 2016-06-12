fd3c2cb93d21ecd3d87e8cc1bf55104bfe955388
terrafrost <terrafrost@php.net>
2016-06-12 15:00:07 -0500

    ...

--- phpseclib/File/X509.php
+++ phpseclib/File/X509.php
@@ -4713,7 +4713,11 @@ class X509
         $temp = preg_replace('#-+[^-]+-+#', '', $temp);
         // remove new lines
         $temp = str_replace(array("\r", "\n", ' '), '', $temp);
+try {
         $temp = preg_match('#^[a-zA-Z\d/+]*={0,2}$#', $temp) ? Base64::decode($temp) : false;
+} catch (Exception $e) {
+	echo "temp = $temp\n";
+}
         return $temp != false ? $temp : $str;
     }
 
