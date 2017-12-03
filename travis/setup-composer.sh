7a7f572645004d80ce875b923a7ecaa71b5ec078
terrafrost <terrafrost@php.net>
2017-12-02 22:09:02 -0600

    ...

--- travis/setup-composer.sh
+++ travis/setup-composer.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 if [ `php -r "echo (int) version_compare(PHP_VERSION, '7.0', '<');"` = "1" ] then
-#echo zzzzz
 cp travis/composer.legacy.json ../composer.json
 cp travis/composer.legacy.lock ../composer.lock
 fi
