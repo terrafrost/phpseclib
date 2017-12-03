2dfe5153fe6ec90de0d97f2639e3d3ff14b0b92f
terrafrost <terrafrost@php.net>
2017-12-02 21:39:30 -0600

    ...

--- travis/setup-composer.sh
+++ travis/setup-composer.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+if [ `php -r "echo (int) version_compare(PHP_VERSION, '7.0', '<');"` = "1" ]
+then
+cp travis/composer.legacy.json ../composer.json
+cp travis/composer.legacy.lock ../composer.lock
+fi
+travis/install-php-extensions.sh; fi"
 composer self-update --no-interaction
 composer install --no-interaction
