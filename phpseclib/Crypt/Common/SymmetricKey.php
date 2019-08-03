343410b1dee34a2bb8145246357e872567e69ba9
terrafrost <terrafrost@php.net>
2019-08-03 11:56:39 -0500

    ...

--- phpseclib/Crypt/Common/SymmetricKey.php
+++ phpseclib/Crypt/Common/SymmetricKey.php
@@ -2735,6 +2735,11 @@ abstract class SymmetricKey
 
         }
 
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+echo $encrypt;
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+exit;
+
         // Before discrediting this, please read the following:
         // @see https://github.com/phpseclib/phpseclib/issues/1293
         // @see https://github.com/phpseclib/phpseclib/pull/1143
