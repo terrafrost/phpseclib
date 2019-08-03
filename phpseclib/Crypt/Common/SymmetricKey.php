43c730f7a3ff65cc9b1f4f2e324a7a2aec3d3234
terrafrost <terrafrost@php.net>
2019-08-03 16:21:28 -0500

    ...

--- phpseclib/Crypt/Common/SymmetricKey.php
+++ phpseclib/Crypt/Common/SymmetricKey.php
@@ -2661,16 +2661,22 @@ $_text = $plaintext;
                 break;
 
         }
-
+/*
 echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 echo $encrypt;
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+exit;
+*/
+$zzz = '$func = function ($_action, $_text) { echo "\n\n\nbbbbbb\n\n\n"; ' . $init_crypt . 'if ($_action == "encrypt") { echo "\n\n\nzzzzz\n\n\n";' . $encrypt . ' echo "\n\n\naaaaaaa\n\n\n";} else { ' . $decrypt . ' }};';
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+echo $zzz;
 echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 exit;
 
         // Before discrediting this, please read the following:
         // @see https://github.com/phpseclib/phpseclib/issues/1293
         // @see https://github.com/phpseclib/phpseclib/pull/1143
-        eval('$func = function ($_action, $_text) { echo "\n\n\nbbbbbb\n\n\n"; ' . $init_crypt . 'if ($_action == "encrypt") { echo "\n\n\nzzzzz\n\n\n";' . $encrypt . ' echo "\n\n\naaaaaaa\n\n\n";} else { ' . $decrypt . ' }};');
+        eval($zzz);
 
         return \Closure::bind($func, $this, static::class);
     }
