e52bc92f07fbe978c9379ae53eebc958459a726d
terrafrost <terrafrost@php.net>
2023-09-21 13:42:25 -0500

    ...

--- test.php
+++ test.php
@@ -5,4 +5,5 @@ $aes = new \phpseclib3\Crypt\AES('cbc');
 $aes->setPreferredEngine('Eval');
 $aes->setKey(str_repeat('x', 16));
 $aes->setIV(str_repeat('x', 16));
+$aes->disablePadding();
 echo bin2hex($aes->decrypt(str_repeat('x', 16)));
