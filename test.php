8d7cb9485e8e1dfac313bc30ab823f485c3b1a84
terrafrost <terrafrost@php.net>
2023-09-21 13:16:10 -0500

    ...

--- test.php
+++ test.php
@@ -1,4 +1,8 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-echo "ZZZZZZZZ";
+$aes = new \phpseclib3\Crypt\AES('ctr');
+$aes->setPreferredEngine('Eval');
+$aes->setKey(str_repeat('x', 16));
+$aes->setIV(str_repeat('x', 16));
+echo bin2hex($aes->encrypt(str_repeat('x', 16)));
